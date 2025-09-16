6bed35fb6fee88363ce742e28a6c67d7b23026c1
Mandeep-Singh18 <mandeepsingh36633@gmail.com>
2025-09-16 21:01:05 +0530

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/api', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
